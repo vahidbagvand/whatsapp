028594f1487dda01e73365f46a047f8b83ef4e64
vahidbagvand <103110394+vahidbagvand@users.noreply.github.com>
2022-07-30 23:13:51 +0430

    Update chat.php

--- chat.php
+++ chat.php
@@ -6,17 +6,11 @@ $error = "Enter the phone number";
 }else{
     
     header("location:https://api.whatsapp.com/send/?phone=98".$_POST['tel']);
-    //echo "<script>window.location.href=''</script>";
-
 }
 }
 ?>
-
-
 <!doctype html>
 <html lang="FA-IR">
-
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
@@ -25,15 +19,9 @@ $error = "Enter the phone number";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title> Go to WhatsApp chat </title>
 </head>
-
-
-
 <body class="p-3 mb-2 bg-warning text-white" style="margin: 40px; border-radius: 20px ; border-radius: 20px ">
-
 <div id="main">
 <div class="p-3 mb-2 bg-info text-white"  >
-
-
     <form class="form-inline" dir="rtl" method="post" >
   <div class="form-group mb-2">
   </div>
