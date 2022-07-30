ad5699c08204c46e713eab2f8fdd28ae1ecd444a
vahidbagvand <103110394+vahidbagvand@users.noreply.github.com>
2022-07-30 23:20:59 +0430

    Update chat.php

--- chat.php
+++ chat.php
@@ -17,7 +17,7 @@ $error = "Enter the phone number";
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title> Go to WhatsApp chat </title>
+    <title>Connect to WhatsApp</title>
 </head>
 <body class="p-3 mb-2 bg-warning text-white" style="margin: 40px; border-radius: 20px ; border-radius: 20px ">
 <div id="main">
