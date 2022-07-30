80ba4e098508090fe8b5028555fabd9876d4d159
vahidbagvand <103110394+vahidbagvand@users.noreply.github.com>
2022-07-30 23:09:29 +0430

    Update chat.php

--- chat.php
+++ chat.php
@@ -1,11 +1,11 @@
 <?php
 
 if( isset($_POST['login']) ){
-if( !isset($_POST['username']) ){
-$error = "شماره تلفن را وارد کنید.";
+if( !isset($_POST['tel']) ){
+$error = "Enter the phone number";
 }else{
     
-    header("location:https://api.whatsapp.com/send/?phone=98".$_POST['username']);
+    header("location:https://api.whatsapp.com/send/?phone=98".$_POST['tel']);
     //echo "<script>window.location.href=''</script>";
 
 }
@@ -23,7 +23,7 @@ $error = "شماره تلفن را وارد کنید.";
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title> رفتن به چت  واتساپ </title>
+    <title> Go to WhatsApp chat </title>
 </head>
 
 
@@ -38,25 +38,15 @@ $error = "شماره تلفن را وارد کنید.";
   <div class="form-group mb-2">
   </div>
   <div class="form-group mx-sm-3 mb-2">
-    <label for="inputPassword2" class="sr-only">شماره تلفن را بدون صفر وارد کنید</label>
-</br></br><input type="text" name="username" class="form-control" id="inputPassword2" placeholder="مثال : 9189323230">
+    <label for="inputPassword2" class="sr-only">Enter the phone number without zeros</label>
+</br></br><input type="text" name="tel" class="form-control" id="inputPassword2" placeholder="مثال : 9189323230">
   </div>
-  </br><center><button name="login" type="submit" class="btn btn-light">بزن بریم</button></center>
+  </br><center><button name="login" type="submit" class="btn btn-light"> Let's go </button></center>
 
 
 </form>
     </div>
-
-
-
 </div>
-<center>
-<br>
-<br>
-تمامی حقوق مادی و معنوی این سایت متعلق به <a class="link-dark" href="https://vahid68i.ir/">وحید بگوند  </a> می باشد 
-</center>
-
 </body>
-
 </div>
 </html>
